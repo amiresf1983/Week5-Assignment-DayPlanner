b1b4e63f4f7df05ec2b980c12d1cac6c95708e3c
Amir Esfandiari <amir.esf1983@gmail.com>
2022-06-21 11:18:26 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,142 +1,76 @@
-let day = [
-  {
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: "",
-  },
-  {
-    id: "1",
-    hour: "10",
-    time: "10",
-    meridiem: "am",
-    reminder: "",
-  },
-  {
-    id: "2",
-    hour: "11",
-    time: "11",
-    meridiem: "am",
-    reminder: "",
-  },
-  {
-    id: "3",
-    hour: "12",
-    time: "12",
-    meridiem: "pm",
-    reminder: "",
-  },
-  {
-    id: "4",
-    hour: "01",
-    time: "13",
-    meridiem: "pm",
-    reminder: "",
-  },
-  {
-    id: "5",
-    hour: "02",
-    time: "14",
-    meridiem: "pm",
-    reminder: "",
-  },
-  {
-    id: "6",
-    hour: "03",
-    time: "15",
-    meridiem: "pm",
-    reminder: "",
-  },
-  {
-    id: "7",
-    hour: "04",
-    time: "16",
-    meridiem: "pm",
-    reminder: "",
-  },
-  {
-    id: "8",
-    hour: "05",
-    time: "17",
-    meridiem: "pm",
-    reminder: "",
-  },
-];
+let slotContainerElEl = $(".container");
+let todayEl = $("#today");
 
-function getDateHead() {
-  let todayHeader = moment().format("dddd, Do MMMM YYYY");
-  $("#dayNow").text(todayHeader);
+let currentDate = moment().format("dddd, Do MMMM YYYY");
+todayEl.text(currentDate);
+
+let hr = parseInt(moment().format("HH"));
+let slotArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+for (let index = 0; index < slotArray.length; index++) {
+  let time = slotArray[index];
+
+  let slot = $("<div>");
+  slot.addClass("row slot");
+  slot.attr("id", time);
+  slotContainerElEl.append(slot);
+
+  let slotTag = $("<Tag>");
+  slotTag.addClass("inputText col-1");
+  slotTag.text(time + ":00");
+  slot.append(slotTag);
+
+  let slotValue = $("<textarea>");
+  slotValue.attr("rows", "1");
+  slotValue.addClass("form-control textarea col-10");
+  slotValue.attr("id", "input" + time);
+
+  if (time < hr) {
+    slotValue.addClass("before");
+  } else if (time === hr) {
+    slotValue.addClass("now");
+  } else {
+    slotValue.addClass("after");
+  }
+  slot.append(slotValue);
+
+  let slotBtn = $("<button>");
+  slotBtn.addClass("saveBtn col-1");
+  slotBtn.attr("row", "2");
+
+  let btnIcon = $("<i>");
+  btnIcon.addClass("fa fa-save");
+  slotBtn.append(btnIcon);
+  slot.append(slotBtn);
 }
 
-function saveTask() {
-  localStorage.setItem("day", JSON.stringify(day));
+function start() {
+  for (let index = 0; index < slotArray.length; index++) {
+    var time = slotArray[index];
+
+    if (localStorage.getItem(time)) {
+      var contentId = "input" + time;
+      var contentSection = $("#" + contentId);
+      contentSection.text(localStorage.getItem(time));
+    }
+  }
+  slotContainerElEl.on("click", "button", saveContent);
 }
 
-function displayTask() {
-  day.forEach(function (_thisHour) {
-    $(`#${_thisHour.id}`).val(_thisHour.reminder);
-  });
-}
-
-function init() {
-  let storedDay = JSON.parse(localStorage.getItem("day"));
-
-  if (storedDay) {
-    day = storedDay;
+function saveContent(event) {
+  if ($(event.target).parent().attr("id")) {
+    var saveIncoming = $(event.target).parent().attr("id");
+    var saveNoteSection = $(event.target).parent().children("textarea");
+  } else {
+    let saveIncoming = $(event.target).parent().parent().attr("id");
+    let saveNoteSection = $(event.target)
+      .parent()
+      .parent()
+      .children("textarea");
   }
 
-  saveTask();
-  displayTask();
+  saveInput = saveNoteSection.val();
+  localStorage.setItem(saveIncoming, saveInput);
 }
 
-getDateHead();
-
-day.forEach(function (thisHour) {
-  let rowHour = $("<form>").attr({
-    class: "row",
-  });
-  $(".container").append(rowHour);
-
-  let fieldHour = $("<div>").text(`${thisHour.hour}${thisHour.meridiem}`).attr({
-    class: "col-md-2 hour",
-  });
-
-  let planHour = $("<div>").attr({
-    class: "col-md-9 state p-0",
-  });
-  let planData = $("<textarea>");
-  planHour.append(planData);
-  planData.attr("id", thisHour.id);
-  if (thisHour.time < moment().format("HH")) {
-    planData.attr({
-      class: "prev",
-    });
-  } else if (thisHour.time === moment().format("HH")) {
-    planData.attr({
-      class: "curr",
-    });
-  } else if (thisHour.time > moment().format("HH")) {
-    planData.attr({
-      class: "next",
-    });
-  }
-
-  let saveButton = $("<i class='far fa-save fa-lg'></i>");
-  let savePlan = $("<button>").attr({
-    class: "col-md-1 btnSave",
-  });
-  savePlan.append(saveButton);
-  rowHour.append(fieldHour, planHour, savePlan);
-});
-
-init();
-
-$(".btnSave").on("click", function (event) {
-  event.preventDefault();
-  let saveIndex = $(this).siblings(".state").children(".next").attr("id");
-  day[saveIndex].reminder = $(this).siblings(".state").children(".next").val();
-  console.log(saveIndex);
-  saveTask();
-  displayTask();
-});
+$(document).ready(start);
